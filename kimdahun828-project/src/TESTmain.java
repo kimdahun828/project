709614170c6a7c48143cb067e2e3cfc3600ec634
GOOTT-06 <GOOTT-06@GOOTT-06-PC>
2019-06-13 12:21:08 +0900

    테스트코드 수정

--- kimdahun828-project/src/TESTmain.java
+++ kimdahun828-project/src/TESTmain.java
@@ -1,4 +1,18 @@
 
 public class TESTmain {
 
+	
+	private void main(String[] args) {
+		
+		int k;
+		
+		k = 8081;
+		
+		String j;
+		j = "Yaho";
+		
+		System.out.println(k);
+		System.out.println(j);
+
+	}
 }
